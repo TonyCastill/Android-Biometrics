182f568a962c75c7c0bab55337ab70892e56b389
tony <starreach33@gmail.com>
2024-10-27 18:40:33 -0600

    Feat :sparkles: (ProfileScreen)(HomeScreen) Implemented a basic app with biometrics

--- app/src/main/java/com/example/biometrics/MainActivity.kt
+++ app/src/main/java/com/example/biometrics/MainActivity.kt
@@ -19,7 +19,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -27,7 +29,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.biometrics.BiometricPromptManager.*
 import com.example.biometrics.ui.theme.BiometricsTheme
 
@@ -57,37 +64,8 @@ class MainActivity : AppCompatActivity() {
         setContent {
             BiometricsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    //Access the flow to collect events
-                    val biometricResult by promptManager.promptResults.collectAsState(initial = null)
 
-                    // In order to fire and launch the activity
-                    // to set a biometric or enroll it
-                    val enrollLauncher = rememberLauncherForActivityResult(
-                        // We fire the StartActivityFor result to pop up an
-                        // activity where the user can choose a pattern
-                        contract = ActivityResultContracts.StartActivityForResult(),
-                        onResult = {
-                            println("Activity result: $it")
-                        }
-                    )
-
-                    // To prompt the user to set a biometric
-                    // signature in case it hasn't been set
-                    LaunchedEffect(biometricResult) {
-                        if(biometricResult is BiometricResult.AuthenticationNotSet){
-                            if(Build.VERSION.SDK_INT >= 30){
-                                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
-                                    putExtra(
-                                        Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
-                                        BIOMETRIC_STRONG or DEVICE_CREDENTIAL
-                                        // Alt + Enter -> import that
-                                    )
-                                }
-                                // Fire the activity
-                                enrollLauncher.launch(enrollIntent)
-                            }
-                        }
-                    }
+                    // ------------------- MAIN CONTENT -------------------
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
@@ -96,44 +74,17 @@ class MainActivity : AppCompatActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally
                     ){
                         //Simple button
-                        Button(onClick = {
+                        /*Button(onClick = {
                             promptManager.showBiometricPrompt(
                                 title = "Sample prompt",
                                 description = "Sample prompt description"
                             )
                         }) {
                             Text(text = "Authenticate")
-                        }
-                        biometricResult?.let{ //like an if after the event
-                            result -> // Do something according to the result
-                            Text(
-                                text = when(result){
-                                    //Alt + Enter -> Add remaining branches
+                        }*/
+                        ComposeMultiScreenApp(promptManager)
 
-                                    //Select the first sentence before the dot
-                                    // Alt+ Enter -> import memembers from ....
-                                    is BiometricResult.AuthenticationError -> {
-                                        result.error
-                                    }
-                                    BiometricResult.AuthenticationFailed -> {
-                                        "Authentication failed"
-                                    }
-                                    BiometricResult.AuthenticationNotSet -> {
-                                        "Authentication not set"
-                                    }
-                                    BiometricResult.AuthenticationSuccess -> {
-                                        "Authentication success"
-                                    }
-                                    BiometricResult.FeatureUnavailable -> {
-                                        "Feature unavailable"
-                                    }
-                                    BiometricResult.HardwareUnavailable -> {
-                                        "Hardware unavailable"
-                                    }
-                                }
-                            )
 
-                        }
                     }
                 }
             }
@@ -142,17 +93,18 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeMultiScreenApp(promptManager:BiometricPromptManager){
+    val navController = rememberNavController()
+    Surface(color= MaterialTheme.colorScheme.surface){
+        SetupNavGraph(navController=navController,promptManager) //función propia //crea el grafo recordando el navcontroller donde nos encontramos
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BiometricsTheme {
-        Greeting("Android")
+fun SetupNavGraph(navController: NavHostController,promptManager:BiometricPromptManager) {
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { HomeScreen(navController,promptManager) }
+        composable("profile") { ProfileScreen(navController) }
+
     }
 }
